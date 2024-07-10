44657539001aefc7e413a836d36f3c0eb203eb66
sophia-bergamo <sophiabergamo7@gmail.com>
2024-07-10 10:39:34 -0300

    wip

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,16 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput , Text, TouchableOpacity} from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello World!</ThemedText>
+      <ThemedView style={styles.titleContainer}> 
+        <ThemedText type="title">Bem vindo(a) Taqtile!</ThemedText>
+        <TextInput style={styles.formInputs} placeholder="E-mail" />
+        <TextInput style={styles.formInputs} placeholder="Senha"/>
+        <TouchableOpacity style={styles.button}>
+        <Text style={styles.textButton}>Entrar</Text>
+      </TouchableOpacity>
       </ThemedView>
   );
 }
@@ -15,5 +20,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 200,
     justifyContent: 'center',
+  },
+  formInputs: {
+    borderColor: '#708090',
+    borderRadius: 10,
+    width: '80%',
+    padding: 15,
+    borderWidth: 1,
+    marginTop: 30,
+    opacity: 0.7
+  },
+  button: {
+    backgroundColor: '#FFB6C1',
+    margin: 25,
+    padding: 15,
+    width: '80%',
+    borderRadius: 15,
+    alignItems: 'center'
+  },
+  textButton: {
+    fontWeight: 'bold',
+    fontSize: 15,
   }
 });
