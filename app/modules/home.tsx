478db38af6ac6eee2b822dc51a5686ebecd4490c
sophia-bergamo <sophiabergamo7@gmail.com>
2024-09-28 15:36:09 -0300

    add loading

--- app/modules/home.tsx
+++ app/modules/home.tsx
@@ -1,6 +1,6 @@
 import { useMutation } from '@apollo/client';
 import { useState } from 'react';
-import { View, TextInput, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { View, TextInput, TouchableOpacity, StyleSheet, Text, ActivityIndicator } from 'react-native';
 import { ThemedText } from '../components/ThemedText';
 import { ThemedView } from '../components/ThemedView';
 import { LOGIN_MUTATION } from '../mutation/login';
@@ -16,6 +16,7 @@ export default function HomeScreen() {
   const [loginSucess, setLoginSucces] = useState('');
   const [loginError, setLoginError] = useState('');
   const [login] = useMutation(LOGIN_MUTATION);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async () => {
     setLoginError('');
@@ -28,6 +29,7 @@ export default function HomeScreen() {
     setPasswordError(passwordValidationError);
 
     if (!emailValidationError || !passwordValidationError) {
+      setLoading(true);
       try {
         const { data } = await login({
           variables: {
@@ -44,6 +46,8 @@ export default function HomeScreen() {
         setLoginSucces('Login realizado com sucesso!');
       } catch (error: any) {
         setLoginError(error.message);
+      } finally {
+        setLoading(false);
       }
     }
   };
@@ -64,9 +68,13 @@ export default function HomeScreen() {
         {passwordError ? <Text style={styles.errorText}>{passwordError}</Text> : null}
       </View>
 
-      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-        <Text style={styles.textButton}>Entrar</Text>
-      </TouchableOpacity>
+      {loading ? (
+        <ActivityIndicator size="large" color="#40E0D0" />
+      ) : (
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.textButton}>Entrar</Text>
+        </TouchableOpacity>
+      )}
 
       {loginSucess ? <Text style={styles.loginSucces}>{loginSucess}</Text> : null}
 
